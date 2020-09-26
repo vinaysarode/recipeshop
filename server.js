4a17cb4c18e3296a148be0f9b20fdfd4114aafac
vinaysarode <vmsarode@gmail.com>
2020-09-26 18:40:31 +0530

    added heroku

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+  
 const express = require('express');
-const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/vinay-recipeshopping'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/vinay-recipeshopping/index.html'));});
+
+app.use(express.static('./dist/recipeshopping'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/recipeshopping/'}),
+);
+
 app.listen(process.env.PORT || 8080);
